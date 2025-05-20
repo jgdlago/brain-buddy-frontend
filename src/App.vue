e3f3fc27f2c14f881cd9c12f838b533f7a9f8f8d
paulodallago <paulohdallago@gmail.com>
2025-05-20 10:45:55 -0300

    create project

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,9 @@
 <script>
-  import Login from './components/Login.vue';
-  import Panel from './components/Panel.vue';
-  import Toolbar from './components/Toolbar.vue';
 
-  export default {
-    name: 'App',
-    components: {
-      Login,
-      Panel,
-      Toolbar
-    }
-  }
 </script>
 
 <template>
-  <div class="main"> 
-      <!-- TODO rever esse main -->
-      <RouterView/>
-  </div>
+
 </template>
 
 <style scoped>
