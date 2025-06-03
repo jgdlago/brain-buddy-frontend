f99766926266976e8a526b28768afd3016b7297c
Paulo Dal Lago <paulohdallago@gmail.com>
2025-06-03 00:13:04 -0300

    fix: Correções gerais

--- src/services/GroupService.js
+++ src/services/GroupService.js
@@ -2,6 +2,5 @@ import Axios from "../axios/api";
 
 export const getGroups = async (userId) => {
   const response = await Axios.get(`/group?responsible_user_id=${userId}`);
-  console.log(response.data.data);
   return response.data.data;
 };
