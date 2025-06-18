977b4c917b78ab4ac792fa3677131e62ef5421cb
JoaoPetucoo <joaopetuco0512@gmail.com>
2025-06-17 23:35:05 -0300

    ♻️ refactor: Ajusta para pegar o novo sidebar

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,36 +1,36 @@
 <script setup>
 import { ref } from 'vue';
 import NotificationDrawer from './NotificationDrawer.vue';
-import Sidebar from './Sidebar.vue';
+import SidebarMenu from './SidebarMenu.vue';
 import { Toolbar } from 'primevue';
 import { useUserStore } from '../stores/userStore';
 const notificationDrawerVisible = ref(false);
-const sidebarVisible = ref(false);
 
 const userStore = useUserStore();
 </script>
 
 <template>
-    <Toolbar class="toolbar"> 
+    <Toolbar class="toolbar">
         <template #start>
-            <i class="pi pi-bars" style="margin-right: 1rem;" @click="sidebarVisible = true"/>
-            Olá, {{userStore.name}} 
+            <i class="pi pi-bars" style="margin-right: 1rem;" @click="sidebarVisible = true" />
+            Olá, {{ userStore.name }}
             <!-- todo -->
         </template>
         <template #end>
-            <i class="pi pi-bell" @click="notificationDrawerVisible = true"/>
+            <i class="pi pi-bell" @click="notificationDrawerVisible = true" />
         </template>
     </ToolBar>
-    <NotificationDrawer v-model:visible="notificationDrawerVisible"/>
-    <Sidebar v-model:visible="sidebarVisible"/>
+    <NotificationDrawer v-model:visible="notificationDrawerVisible" />
+    
+    <SidebarMenu />
 </template>
 
 <style scoped>
-    .toolbar {
-        border: 0px;
-        border-radius: 0;
-        background-color: var(--p-primary-header);
-        color: var(--p-primary-inverse-color);
-        box-shadow: var(--shadow-heavy);
-    }
+.toolbar {
+    border: 0px;
+    border-radius: 0;
+    background-color: var(--p-primary-header);
+    color: var(--p-primary-inverse-color);
+    box-shadow: var(--shadow-heavy);
+}
 </style>
