f3d9707d8fc00ab64fc4918badd9601895380165
JoaoPetucoo <joaopetuco0512@gmail.com>
2025-06-22 22:21:56 -0300

    ♻️ refactor: ajusta o layout do sistema

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,9 +1,21 @@
+<!-- layouts/AppLayout.vue -->
 <script setup>
-import Toolbar from '../components/Toolbar.vue';
-
+import SidebarMenu from '../components/SidebarMenu.vue';
+import Header from '../components/Header.vue';
 </script>
 
 <template>
-    <Toolbar/>
-    <RouterView/>
+    <div class="min-h-screen bg-gray-50 flex flex-col">
+        <Header />
+
+        <div class="flex-1 flex overflow-hidden">
+            <SidebarMenu />
+
+            <main class="flex-1 overflow-y-auto">
+                <div class="p-2">
+                    <RouterView />
+                </div>
+            </main>
+        </div>
+    </div>
 </template>
